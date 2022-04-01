57f5f01747dc544dc43dffe2a2f3781462e9faa0
olodii <87850578+olodii@users.noreply.github.com>
2022-04-01 20:48:18 +0900

    Update [04]data_analysis_otherGreen.R

--- [04]data_analysis_otherGreen.R
+++ [04]data_analysis_otherGreen.R
@@ -25,29 +25,29 @@ par(mfrow=c(1,1)); boxplot(exp)
 x11();par(mfrow=c(1,1)); corrplot::corrplot(cor(exp),method="square",type="upper",addCoef.col = TRUE,tl.col="black")
 
 # 다른 변수
-cov <- dat %>% dplyr::select(mtemp_1T:mhumi_6m,d_ma_age,mbmi,d_b_wt,d_ga_wks,edu_university,shs,sex,green100:green234_10000)
+cov <- dat %>% dplyr::select(mtemp_1T:mhumi_6m,age,mbi,dbwt,wks,medu,shs,sex,green:green4)
 
 # 건강 영향
-nd <- dat %>% dplyr::select(y_cogqo,y_motqo)
+nd <- dat %>% dplyr::select(yc,ym)
 
 dat2 <- cbind(exp, cov, nd)
 
 # 녹지 데이터분포 변환
-dat2 %>% select(green100:green234_10000) %>% summary()
-dat2$lng100m <- log1p(dat2$green100)
-dat2$lng200m <- log1p(dat2$green234_10000)
-dat2$lng300m <- log(dat2$green300)
-dat2$lng500m <- log(dat2$green500)
+dat2 %>% select(green:green4) %>% summary()
+dat2$lng100m <- log1p(dat2$green)
+dat2$lng200m <- log1p(dat2$green2)
+dat2$lng300m <- log(dat2$green3)
+dat2$lng500m <- log(dat2$green5)
 dat2 %>% select(lng100m:lng500m) %>% summary()
 
-dat2$edu_university <- as.numeric(dat2$edu_university)
+dat2$medu <- as.numeric(dat2$medu)
 dat2$shs <- as.numeric(dat2$shs)
 dat2$sex <- as.numeric(dat2$sex)
-dat2$d_ma_age <- as.numeric(dat2$d_ma_age)
-dat2$d_b_wt <- as.numeric(dat2$d_b_wt *0.001)
-dat2$d_ga_wks <- as.numeric(dat2$d_ga_wks)
-dat2$y_cogqo <- as.numeric(dat2$y_cogqo)
-dat2$y_motqo <- as.numeric(dat2$y_motqo)
+dat2$age <- as.numeric(dat2$age)
+dat2$dbwt <- as.numeric(dat2$dbwt *0.001)
+dat2$wks <- as.numeric(dat2$wks)
+dat2$yc <- as.numeric(dat2$yc)
+dat2$ym <- as.numeric(dat2$ym)
 
 which(names(dat2)==c("pm25_6m.u"))
 
@@ -60,32 +60,31 @@ d_dat<-dat2
 #d_dat <- data.frame(predict(dummy, newdata=dat2))
 
 D1 <- d_dat %>% dplyr::select(y_cogqo, y_motqo, pm10_preg.u, no2_preg.u, pm25_preg.u,
-                             mtemp_preg, mhumi_preg, d_ma_age, d_ga_wks, d_b_wt, mbmi, edu_university:sex,
+                             mtemp_preg, mhumi_preg, age, wks, dbwt, mbi, medu:sex,
                              lng100m:lng500m)
 D2 <- d_dat %>% dplyr::select(y_cogqo, y_motqo, pm10_1T.u, no2_1T.u, pm25_1T.u,
-                             mtemp_1T, mhumi_1T, d_ma_age, d_ga_wks, d_b_wt, mbmi, edu_university:sex,
+                             mtemp_1T, mhumi_1T, age, wks, dbwt, mbi, medu:sex,
                              lng100m:lng500m)
 D3 <- d_dat %>% dplyr::select(y_cogqo, y_motqo, pm10_2T.u, no2_2T.u, pm25_2T.u,
-                             mtemp_2T, mhumi_2T, d_ma_age, d_ga_wks, d_b_wt, mbmi, edu_university:sex,
+                             mtemp_2T, mhumi_2T, age, wks, dbwt, mbi, medu:sex,
                              lng100m:lng500m)
 D4 <- d_dat %>% dplyr::select(y_cogqo, y_motqo, pm10_3T.u, no2_3T.u, pm25_3T.u,
-                             mtemp_3T, mhumi_3T, d_ma_age, d_ga_wks, d_b_wt, mbmi, edu_university:sex,
+                             mtemp_3T, mhumi_3T, age, wks, dbwt, mbi, medu:sex,
                              lng100m:lng500m)
 D5 <- d_dat %>% dplyr::select(y_cogqo, y_motqo, pm10_6m.u, no2_6m.u, pm25_6m.u,
-                             mtemp_6m, mhumi_6m, d_ma_age, d_ga_wks, d_b_wt, mbmi, edu_university:sex,
+                             mtemp_6m, mhumi_6m, age, wks, dbwt, mbi, medu:sex,
                              lng100m:lng500m)
 
-
-# preganacy, 1y 407명
+#
 dat <- Data[[2]]
 
 exp <- dat %>% dplyr::select(pm10_1T.u:pm25_1yc.u)
 
 # 보정변수
-cov <- dat %>% dplyr::select(mtemp_1T:mhumi_1y, d_ma_age,mbmi,d_b_wt,d_ga_wks,edu_university,shs,sex,green100:green234_10000)
+cov <- dat %>% dplyr::select(mtemp_1T:mhumi_1y, age, wks, dbwt, mbi, medu:sex,green:green4)
 
 # 건강영향
-nd <- dat %>% dplyr::select(y1_cogqo,y1_motqo)
+nd <- dat %>% dplyr::select(yc1,ym1)
 
 dat2 <- cbind(exp, cov, nd)
 
@@ -95,56 +94,57 @@ dat2$lng200m <- log1p(dat2$green234_10000)
 dat2$lng300m <- log(dat2$green300)
 dat2$lng500m <- log(dat2$green500)
 
-dat2$edu_university <- as.numeric(dat2$edu_university)
+dat2$medu <- as.numeric(dat2$medu)
 dat2$shs <- as.numeric(dat2$shs)
 dat2$sex <- as.numeric(dat2$sex)
-dat2$d_ma_age <- as.numeric(dat2$d_ma_age)
-dat2$d_b_wt <- as.numeric(dat2$d_b_wt *0.001)
-dat2$d_ga_wks <- as.numeric(dat2$d_ga_wks)
-dat2$y1_cogqo <- as.numeric(dat2$y1_cogqo)
-dat2$y1_motqo <- as.numeric(dat2$y1_motqo)
+dat2$age <- as.numeric(dat2$age)
+dat2$dbwt <- as.numeric(dat2$dbwt *0.001)
+dat2$wks <- as.numeric(dat2$wks)
+dat2$yc1 <- as.numeric(dat2$yc1)
+dat2$ym1 <- as.numeric(dat2$ym1)
 
 which(names(dat2)==c("pm25_1yc.u"))
+
 #표준화
 dat2[,1:20] <- scale(dat2[,1:20], center=T, scale=T)
 d_dat<-dat2
 
 D6 <- d_dat %>% dplyr::select(y1_cogqo, y1_motqo, pm10_1yc.u, no2_1yc.u, pm25_1yc.u,
-                             mtemp_1y, mhumi_1y, d_ma_age, d_ga_wks, d_b_wt, mbmi, edu_university:sex,
+                             mtemp_1y, mhumi_1y, age, wks, dbwt, mbi, medu:sex,
                              lng100m:lng500m)
 D7 <- d_dat %>% dplyr::select(y1_cogqo, y1_motqo, pm10_preg.u, no2_preg.u, 
-                             mtemp_preg, mhumi_preg, d_ma_age, d_ga_wks, d_b_wt, mbmi, edu_university:sex,
+                             mtemp_preg, mhumi_preg, age, wks, dbwt, mbi, medu:sex,
                              lng100m:lng500m)
 
 
-# preganacy, 2y 258명
+#
 dat <- Data[[3]]
 
 exp <- dat %>% dplyr::select(pm10_1T.u:pm25_2yc.u)
 
 # 보정변수
-cov <- dat %>% dplyr::select(mtemp_1T:mhumi_2y,mbmi,d_ma_age,d_b_wt,d_ga_wks,edu_university,shs,sex,green100:green234_10000)
+cov <- dat %>% dplyr::select(mtemp_1T:mhumi_2y, age, wks, dbwt, mbi, medu:sex,green:green4)
 
 # 건강영향
-nd <- dat %>% dplyr::select(y2_cogqo,y2_motqo)
+nd <- dat %>% dplyr::select(yc2,ym2)
 
 dat2 <- cbind(exp, cov, nd)
 
 # 녹지 데이터분포 변환
-dat2$lng100m <- log1p(dat2$green100)
-dat2$lng200m <- log1p(dat2$green234_10000)
-dat2$lng300m <- log(dat2$green300)
-dat2$lng500m <- log(dat2$green500)
+dat2$lng100m <- log1p(dat2$green)
+dat2$lng200m <- log1p(dat2$green2)
+dat2$lng300m <- log(dat2$green3)
+dat2$lng500m <- log(dat2$green4)
 
 
-dat2$edu_university <- as.numeric(dat2$edu_university)
+dat2$medu <- as.numeric(dat2$medu)
 dat2$shs <- as.numeric(dat2$shs)
 dat2$sex <- as.numeric(dat2$sex)
-dat2$d_ma_age <- as.numeric(dat2$d_ma_age)
-dat2$d_b_wt <- as.numeric(dat2$d_b_wt *0.001)
-dat2$d_ga_wks <- as.numeric(dat2$d_ga_wks)
-dat2$y2_cogqo <- as.numeric(dat2$y2_cogqo)
-dat2$y2_motqo <- as.numeric(dat2$y2_motqo)
+dat2$age <- as.numeric(dat2$age)
+dat2$dbwt <- as.numeric(dat2$dbwt *0.001)
+dat2$wks <- as.numeric(dat2$wks)
+dat2$yc2 <- as.numeric(dat2$yc2)
+dat2$ym2 <- as.numeric(dat2$ym2)
 
 which(names(dat2)==c("pm25_2yc.u"))
 
@@ -153,10 +153,10 @@ dat2[,1:21] <- scale(dat2[,1:21], center=T, scale=T)
 d_dat<-dat2
 
 D8 <- d_dat %>% dplyr::select(y2_cogqo, y2_motqo, pm10_2yc.u, no2_2yc.u, pm25_2yc.u,
-                             mtemp_2y, mhumi_2y, d_ma_age, d_ga_wks, d_b_wt, mbmi, edu_university:sex,
+                             mtemp_2y, mhumi_2y, age, wks, dbwt, mbi, medu:sex,
                              lng100m:lng500m)
 D9 <- d_dat %>% dplyr::select(y2_cogqo, y2_motqo, pm10_preg.u, no2_preg.u, 
-                             mtemp_preg, mhumi_preg, d_ma_age, d_ga_wks, d_b_wt, mbmi, edu_university:sex,
+                             mtemp_preg, mhumi_preg, age, wks, dbwt, mbi, medu:sex,
                              lng100m:lng500m)
 
 
